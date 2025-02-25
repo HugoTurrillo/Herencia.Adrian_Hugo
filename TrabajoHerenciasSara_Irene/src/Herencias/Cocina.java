3d0bfa5b7a50ec86a9b05ce2dc19220ea982d76c
Adrian <adri.cc1826@gmail.com>
2025-02-25 11:58:47 +0100

    Comienzo del Proyecto

---
+++ TrabajoHerenciasSara_Irene/src/Herencias/Cocina.java
@@ -0,0 +1,11 @@
+package Herencias;
+
+public class Cocina extends Casa {
+
+	public Cocina(double metrosCuadrados, int numPuertas, int numVentanas) {
+	     super("Cocina", metrosCuadrados, numPuertas, numVentanas);
+	    
+	}
+
+	
+}
